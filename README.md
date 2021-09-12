ec2013d347616ce4850576df48de6027128e3ab9
gunmokoo <minju1348@gmail.com>
2021-09-12 22:51:22 +0900

    grid page complete, Responsive main page, create search and click event in main page
    grid page complete, Responsive main page, create search and click event in main page.

--- README.md
+++ README.md
@@ -7,8 +7,6 @@ content
 
 plan
 
-- search in header
-- mobile web < 1100px < pc web
+- next category layout
 - Hover status animations for categories with js
 - location, email, name in footer ex) google
-- section click event
--- js/index.js
+++ js/index.js
@@ -3,7 +3,6 @@ const category = document.getElementsByClassName("page__box");
 
 function searchHandle() {
   search.value = search.value.toUpperCase();
-
   for (let i = 0; i < category.length; i++) {
     const contents = category[i].getElementsByClassName("page__box__contents");
     if (contents[0].innerText.indexOf(search.value) != -1) {
@@ -13,5 +12,16 @@ function searchHandle() {
     }
   }
 }
+function pageMove(event) {
+  if (event.target.innerText === "GRID") {
+    location.href = "grid.html";
+  }
+  if (event.target.innerText === "LAYOUT") {
+    location.href = "layout.html";
+  }
+}
 
 search.addEventListener("keyup", searchHandle);
+for (let i = 0; i < category.length; i++) {
+  category[i].addEventListener("click", pageMove);
+}
